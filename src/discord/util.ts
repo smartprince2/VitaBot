66129d64b4282ca91784c55cf9ddf17fb11a4c9b
Not Thomiz <jeanouina@lightcord.org>
2021-09-14 08:41:57 +0000

    no spaces anymore between mentions

--- src/discord/util.ts
+++ src/discord/util.ts
@@ -11,20 +11,25 @@ export function generateDefaultEmbed(){
 }
 
 const USER_PATTERN = /^<@!?(?<id>\d{17,19})>$/
+const USER_PATTERN_MULTI = /<@!?\d{17,19}>/g
 const ID_PATTERN = /^\d{17,19}$/
 const ROLE_PATTERN = /^<@&?(?<id>\d{17,19})>$/
+const ROLE_PATTERN_MULTI = /<@&?\d{17,19}>/g
 export function isDiscordUserArgument(arg: string){
-    return USER_PATTERN.test(arg) || ID_PATTERN.test(arg) || ROLE_PATTERN.test(arg)
+    return USER_PATTERN_MULTI.test(arg) || ID_PATTERN.test(arg) || ROLE_PATTERN_MULTI.test(arg)
 }
 export async function parseDiscordUser(arg: string){
-    if(USER_PATTERN.test(arg)){
-        const parsed = arg.match(USER_PATTERN)
-        try{
-            const user = await client.users.fetch(parsed.groups.id)
-            return [user]
-        }catch{
-            return []
+    if(USER_PATTERN_MULTI.test(arg)){
+        const matches = arg.match(USER_PATTERN_MULTI)
+        const ids:string[] = []
+        for(const match of matches){
+            const parsed = match.match(USER_PATTERN)
+            if(ids.includes(parsed.groups.id))continue
+            ids.push(parsed.groups.id)
         }
+        return (await Promise.all([...ids].map(id => {
+            return client.users.fetch(id).catch(() => null)
+        }))).filter(e => !!e)
     }else if(ID_PATTERN.test(arg)){
         try{
             const user = await client.users.fetch(arg)
@@ -33,18 +38,26 @@ export async function parseDiscordUser(arg: string){
             return []
         }
     }else if(ROLE_PATTERN.test(arg)){
-        const parsed = arg.match(ROLE_PATTERN)
-        try{
-            const guild = client.guilds.cache.find(e => e.roles.cache.has(parsed.groups.id))
-            if(!guild)return []
-            const members = await guild.members.fetch()
-            return members.filter(e => 
-                e.roles.cache.has(parsed.groups.id) && 
-                !e.user.bot
-            ).map(e => e.user)
-        }catch{
-            return []
+        const matches = arg.match(ROLE_PATTERN_MULTI)
+        const ids:string[] = []
+        for(const match of matches){
+            const parsed = match.match(ROLE_PATTERN)
+            if(ids.includes(parsed.groups.id))continue
+            ids.push(parsed.groups.id)
         }
+        return (await Promise.all([...ids].map(async id => {
+            try{
+                const guild = client.guilds.cache.find(e => e.roles.cache.has(id))
+                if(!guild)return []
+                const members = await guild.members.fetch()
+                return members.filter(e => 
+                    e.roles.cache.has(id) && 
+                    !e.user.bot
+                ).map(e => e.user)
+            }catch{
+                return []
+            }
+        }))).flat().filter(e => !!e)
     }
     return []
 }
