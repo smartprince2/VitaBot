b82b9e7d88bf95ba83c3538b9b15fc85c1bdf8c8
Not Thomiz <jeanouina@lightcord.org>
2021-09-24 16:02:54 +0000

    [SBP] remove old code

--- src/sbpvote/index.ts
+++ src/sbpvote/index.ts
@@ -125,35 +125,3 @@ Promise.all([
         })
     })
 })
-
-/*
-requestWallet("get_sbp_votes", "VitaminCoinSBP")
-.then((votes:{
-    name: string,
-    total: string,
-    votes: {
-        [address: string]: string
-    }
-}) => {
-    const addresses = {}
-    const total = new BigNumber(votes.total)
-    const viteTotal = new BigNumber(695).shiftedBy(18)
-    for(const address in votes.votes){
-        const viteAmount = new BigNumber(new BigNumber(votes.votes[address])
-            .div(total)
-            .times(viteTotal)
-            .toString()
-            .split(".")[0])
-            .shiftedBy(-18)
-            .toFixed()
-        addresses[address] = viteAmount
-    }
-    console.log(JSON.stringify(addresses, null, 4))
-    let totalVite = 0
-    for(const address in addresses){
-        const amount = parseFloat(addresses[address])
-        totalVite = totalVite+amount
-    }
-    console.log(JSON.stringify(totalVite, null, 4))
-})
-*/
